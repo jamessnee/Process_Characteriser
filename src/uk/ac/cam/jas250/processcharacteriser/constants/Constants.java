6a82039822b6167cc0f59bf5c69844484e0e6927
James Snee <james.snee@gmail.com>
2012-07-31 18:27:24 +0100

    Added a constants file which defines the window size for the experimental run. Should aid the one-click experiment run approach.

---
+++ src/uk/ac/cam/jas250/processcharacteriser/constants/Constants.java
@@ -0,0 +1,11 @@
+/*
+ * 
+ * A convenient place to put experiment constants. Useful for a 'one-click' experiment.
+ * 
+ */
+
+package uk.ac.cam.jas250.processcharacteriser.constants;
+
+public final class Constants {
+	public final int WINDOW_SIZE = 5;
+}
